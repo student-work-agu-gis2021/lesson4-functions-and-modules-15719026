aa524bcca74f1f616bf20b9f1fcec10831fb19f6
15719026 <>
2021-05-09 16:51:31 +0000

    converts temperature

--- Exercise_4_problem_1.py
+++ Exercise_4_problem_1.py
@@ -19,7 +19,7 @@
 # - Adding comments in your code and a docstring that explains how to use your `fahr_to_celsius` function (i.e., you should write the purpose of the function, parameters, and returned values)
 
 # YOUR CODE HERE
-
+def fahr_to_celsius(temp_fahrenheit):
 # ### Problem 1 tests
 # 
 # Check that the function produces correct answers for:
